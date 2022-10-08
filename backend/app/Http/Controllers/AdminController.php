eb19bdcc925156f71310ee2cb7ab32716c9da1d9
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-09 00:44:05 +0300

    backend: add addStudent api

---
+++ backend/app/Http/Controllers/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Course;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function addStudent (Request $request) {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'user_type' => $request->user_type,
+        ]);
+        
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Student added successfully',
+            'user' => $user,
+        ], 200);
+    }
+
+}
