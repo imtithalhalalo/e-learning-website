693e74435652be5b281cee43ac8437c582b43976
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-09 00:46:58 +0300

    backend: create course model

---
+++ backend/app/Models/Course.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+class Course extends Eloquent implements JWTSubject
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'desc',
+        'image_url'
+    ];
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
