91efcd3361de51be96950a299db8a2531f353582
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-09 00:55:43 +0300

    frontend: link login axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Register from './components/Register';
+import Login from './components/Login';
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from 'axios';
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [courses, setCourses] = useState([]);
   const register = async (user) => {
     const data ={
       name: user.name,
@@ -23,11 +25,26 @@ function App() {
     window.location.pathname = '/login';
   };
 
+  const login = async (user) => {
+    const data ={
+      email: user.email,
+      password: user.password
+    }
+
+    await axios.post("http://127.0.0.1:8000/api/v0.1/login", data).then(response=>{
+        if(response.data.user.user_type === 'admin') {
+          localStorage.setItem("token", response.data.authorisation.token);
+          window.location.pathname = '/admin_add_person';
+        }
+    });
+  };
+
   return (
 
       <BrowserRouter>
         <Routes>
           <Route path="/register" element={<Register onAdd={ register }/>} />
+          <Route path="/login" element={<Login onAdd={ login }/>} />
         </Routes>
       </BrowserRouter>
       
