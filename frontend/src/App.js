8d6e9d1f38ab6646801d6db5b6024c7edc691d55
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-09 00:53:54 +0300

    frontend: link register axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Register from './components/Register';
+
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from 'axios';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const register = async (user) => {
+    const data ={
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      user_type: user.user_type
+    }
+
+    await axios.post("http://127.0.0.1:8000/api/v0.1/register", data).then(response=>{
+        const res = response.data;
+        setUsers([...users, res]);
+    });
+    
+    window.location.pathname = '/login';
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register onAdd={ register }/>} />
+        </Routes>
+      </BrowserRouter>
+      
   );
 }
 
