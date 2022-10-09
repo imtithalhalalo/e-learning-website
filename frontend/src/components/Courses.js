116282cc84e9e6d2c572c33aaa55c443ad1fd487
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-09 15:55:04 +0300

    frontend: link get Instructors axios

--- frontend/src/components/Courses.js
+++ frontend/src/components/Courses.js
@@ -4,7 +4,9 @@ import Button from './Button';
 
 const Courses = () => {
   const [ courses, setCourses ] = useState([])
-  
+  const [ instructors, setInstructors ] = useState([])
+  const [ instructorId, setInstructorId ] = useState([])
+  const [ courseName, setCourseName ] = useState([])
 const getCourses = async () => {
   try {
     const res = await axios.get("http://127.0.0.1:8000/api/v0.1/retrievecourses")
@@ -16,9 +18,19 @@ const getCourses = async () => {
   }
 };
 
+const getInstructors = async () => {
+    try {
+  const res = await axios.get("http://127.0.0.1:8000/api/v0.1/getinstructors")
+      
+      setInstructors(res.data);  // set State
+    
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
 
   useEffect(()=>{
-    
+    getInstructors()
     getCourses()
   },[])  
 return (
@@ -32,6 +44,20 @@ return (
          <p>{course.title} </p>
          <Button color={""} text={"Assign Instructor"} />
          </div>
+         
+         <div className="form-control">
+            
+                <select className="choose" name="user_type" onChange={
+                    (e) => {setInstructorId(e.target.value);
+                    setCourseName(course.title);
+                }}>
+                {instructors.map(instructor => (
+                    <option value={instructor._id}>{instructor.name}</option>
+                ))}
+                </select>
+            
+            
+          </div>
          </li>
        ))}
      </ul>
