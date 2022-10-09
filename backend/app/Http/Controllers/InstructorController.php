bb5e7b1b65d709ae49ef68fa3245c75004fcd622
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-09 19:38:50 +0300

    backend: add student to course api

--- backend/app/Http/Controllers/InstructorController.php
+++ backend/app/Http/Controllers/InstructorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use App\Models\Course;
 
 class InstructorController extends Controller
 {
@@ -21,5 +22,21 @@ class InstructorController extends Controller
             'message' => 'Student added successfully',
             'user' => $user,
         ], 200);
+    }
+    
+    public function addStudentToCourse(Request $request) {
+        $student_email = $request->email;
+        
+        $course_id = $request->course_id;
+        
+        $add_student_to_course = Course::where('_id', '=', $course_id)
+                                    ->get();
+        
+        foreach ($add_student_to_course as $row) {
+            $row->update(['student_email' => $student_email]);
+        }
+        return response()->json( 
+            $add_student_to_course,
+        );                
     }
 }
