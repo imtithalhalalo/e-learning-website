653252bb194094fb2b9b2259f9bf8faab0e82e3a
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-09 17:07:57 +0300

    frontend: update navbar component

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,25 +1,21 @@
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({text1 , text2 , text3, path1, path2, path3} ) => {
   return (
     <section className="header background">
     <div className="nav-links">
-    
-        <Link to={"/admin_add_person"}>
-        
-            
-                <span>Add Student Or Instructor</span>
-
-            
+        <img src={process.env.PUBLIC_URL+"images/e-learning-logo.png"} alt=""
+          width="48px"
+          height="48px"/>
+        <Link to={path1}>
+          <span>{text1}</span>
         </Link>
-        <Link to={"/admin_add_course"}>
-
-            
-                <span>Add Course</span>
-            
-            
+        <Link to={path2}>
+          <span>{text2}</span>
         </Link>
-        
+        <Link to={path3}>
+          <span>{text3}</span>
+        </Link>
     </div>
         
     </section>
