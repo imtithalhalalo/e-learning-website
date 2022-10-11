3c5d8500441774c49183585520b9b01622452efd
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-11 23:55:07 +0300

    backend: modify route name

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,7 +11,7 @@ Route::group(["prefix"=> "v0.1"], function(){
     //admin routes
     Route::group(["middleware" => "admin.role"], function(){
         Route::post('/addinstructor', [AdminController::class, 'addInstructor'])->name('add-instructor');
-        Route::post('/addstudent', [AdminController::class, 'addStudent'])->name('add-student');
+        Route::post('/addstudentt', [AdminController::class, 'addStudent'])->name('add-student');
         Route::post('/addcourse', [AdminController::class, 'addCourse'])->name('add-course');
         Route::post('/assigninstructor', [AdminController::class, 'assignInstructor'])->name('assign-instructor-to-Course');
     });
