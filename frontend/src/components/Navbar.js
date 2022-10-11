724228e22f6119784fdcc53b58488dfb54f99f14
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-11 20:14:54 +0300

    frontend: modify navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,23 +5,29 @@ const Navbar = ({User, text1 , text2 , text3, text4, path1, path2, path3, path4}
     <section className="header background">
     <div className="nav-links">
         <div className="bar">
+        <h2>{User}</h2>
         <img src={process.env.PUBLIC_URL+"images/e-learning-logo.png"} alt=""
           width="48px"
           height="48px"/>
-        <h2>{User}</h2>
         </div>
         
         <Link to={path1}>
-          <span>{text1}</span>
+          <img src={process.env.PUBLIC_URL+"images/courses.png"} alt=""
+            width="28px"
+            height="28px"/>
+          <span><h4>{text1}</h4></span>
         </Link>
         <Link to={path2}>
-          <span>{text2}</span>
+          <img src={process.env.PUBLIC_URL+"images/courses.png"} alt=""
+            width="28px"
+            height="28px"/>
+          <span><h4>{text2}</h4></span>
         </Link>
         <Link to={path3}>
-          <span>{text3}</span>
+          <span><h4>{text3}</h4></span>
         </Link>
         <Link to={path4}>
-          <span>{text4}</span>
+          <span><h4>{text4}</h4></span>
         </Link>
     </div>
         
