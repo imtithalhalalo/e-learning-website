bc95e3385c905b47359b63d5681c1f41c6da8be4
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-11 20:17:17 +0300

    frontend: change colors

--- frontend/src/components/CreateAnnouncement.js
+++ frontend/src/components/CreateAnnouncement.js
@@ -2,92 +2,98 @@ import { useState, useEffect } from "react";
 import axios from 'axios'
 
 const CreateAnnouncement = () => {
-    const [title, setTitle] = useState('');
-    const [desc, setDesc] = useState('');
-    const [courseId, setCourseId] = useState('');
-    const [courses, setCourses ] = useState([])
-    const getCourses = async () => {
-        try {
-              const res = await axios.get("http://127.0.0.1:8000/api/v0.1/retrievecourses")
-              
-              setCourses(res.data);
-        
-        } catch (err) {
-          console.error(err.message);
-        }
-    };
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        if (!title || !desc ) {
-          alert("Fill All Fields");
-          return;
-        }
-        console.log(title, desc)
-        
-        
-        setTitle("");
-        setDesc("");
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
+  const [courseId, setCourseId] = useState('');
+  const [courses, setCourses] = useState([])
+  const getCourses = async () => {
+    try {
+      const res = await axios.get("http://127.0.0.1:8000/api/v0.1/retrievecourses")
 
-        const data = {
-            title: title,
-            desc: desc,
-            course_id: courseId
-        }
-        console.log(data)
-        try {
-            await axios.post("http://127.0.0.1:8000/api/v0.1/createannouncement", data,
-            { headers: {'Authorization': `Bearer ${localStorage.getItem(`token`)}`}}).then(response=>{
-               console.log(response);
-            });
-    
-            console.log('success')  
-        } catch (err) {
-            console.error(err.message);
-        }
-        
-    };
-    useEffect(()=>{
-        getCourses()
-    },[]) 
-    return (
-      <main className="container-add">
-        <form className="add-form" onSubmit={onSubmit}>
-          <h2>Add Announcement</h2>
-          <div className="form-control">
-            <label>Title</label>
-            <input
-              type="text"
-              placeholder="Title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </div>
+      setCourses(res.data);
 
-          <div className="form-control">
-            <label>Description</label>
-            <textarea
-              type="text"
-              placeholder="Enter Description"
-              value={desc}
-              onChange={(e) => setDesc(e.target.value)}
-              rows={10}
-            ></textarea>
-          </div>
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (!title || !desc) {
+      alert("Fill All Fields");
+      return;
+    }
+    console.log(title, desc)
 
-          <div className="form-control">
-            <label>Choose Course</label>
-            <select className="choose" name="user_type" onChange={
-                    (e) => {setCourseId(e.target.value);
-                }}>
-                {courses.map(course => (
-                    <option value={course._id}>{course.title}</option>
-                ))}
-                </select>
-          </div>
-          <input type={"submit"} value="Add" className="btn btn-block" />
-        </form>
+
+    setTitle("");
+    setDesc("");
+
+    const data = {
+      title: title,
+      desc: desc,
+      course_id: courseId
+    }
+    console.log(data)
+    try {
+      await axios.post("http://127.0.0.1:8000/api/v0.1/createannouncement", data,
+        { headers: { 'Authorization': `Bearer ${localStorage.getItem(`token`)}` } }).then(response => {
+          console.log(response);
+        });
+
+      console.log('success')
+    } catch (err) {
+      console.error(err.message);
+    }
+
+  };
+  useEffect(() => {
+    getCourses()
+  }, [])
+  return (
+    <main className="container-add">
+      <form className="add-form" onSubmit={onSubmit}>
+        <div className="bar">
+          <h2>Add Announcement </h2>
+          <img src={process.env.PUBLIC_URL + "images/announcement.png"} alt=""
+            width="28px"
+            height="28px" />
+        </div>
+        <div className="form-control">
+          <label>Title</label>
+          <input
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+
+        <div className="form-control">
+          <label>Description</label>
+          <textarea
+            type="text"
+            placeholder="Enter Description"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+            rows={10}
+          ></textarea>
+        </div>
+
+        <div className="form-control">
+          <label>Choose Course</label>
+          <select className="choose" name="user_type" onChange={
+            (e) => {
+              setCourseId(e.target.value);
+            }}>
+            {courses.map(course => (
+              <option value={course._id}>{course.title}</option>
+            ))}
+          </select>
+        </div>
+        <input type={"submit"} value="Add" className="btn btn-block" />
+      </form>
     </main>
-    )
+  )
 }
 
 export default CreateAnnouncement
